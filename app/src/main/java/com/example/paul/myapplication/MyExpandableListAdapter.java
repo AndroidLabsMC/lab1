e2131320cbec61170840ea4ca077f2daca3f2e0d
PaulChege <pchegenjenga@gmail.com>
2017-05-07 22:35:53 +0300

    expandable list view

---
+++ app/src/main/java/com/example/paul/myapplication/MyExpandableListAdapter.java
@@ -0,0 +1,10 @@
+package com.example.paul.myapplication;
+
+import android.widget.BaseExpandableListAdapter;
+
+/**
+ * Created by paul on 07/05/2017.
+ */
+
+public class MyExpandableListAdapter extends BaseExpandableListAdapter {
+}
