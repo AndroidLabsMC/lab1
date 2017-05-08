69124bacc7feb4237c92ff30430588947daa20f2
Clive Makamara <cmosh@live.com>
2017-05-08 05:53:14 +0300

    Remove popup button

--- app/src/main/java/com/example/paul/myapplication/MainActivity.java
+++ app/src/main/java/com/example/paul/myapplication/MainActivity.java
@@ -38,31 +38,7 @@ ListView listView;
             }
         });
 
-        button1 = (Button) findViewById(R.id.button);
-        button1.setOnClickListener(new View.OnClickListener() {
-                                       @Override
-                                       public void onClick(View v) {
-                                           //Creating the instance of PopupMenu
-                                           PopupMenu popup = new PopupMenu(MainActivity.this, button1);
-                                           //Inflating the Popup using xml file
-                                           popup.getMenuInflater()
-                                                   .inflate(R.menu.popup, popup.getMenu());
 
-                                           //registering popup with OnMenuItemClickListener
-                                           popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                                               public boolean onMenuItemClick(MenuItem item) {
-                                                   Toast.makeText(
-                                                           MainActivity.this,
-                                                           "You Clicked : " + item.getTitle(),
-                                                           Toast.LENGTH_SHORT
-                                                   ).show();
-                                                   return true;
-                                               }
-                                           });
-
-                                           popup.show(); //showing popup menu
-                                       }
-                                   });
         String[] values = new String[] { "Item 1",
                 "Item 2",
                 "Item 3",
